bbee892e7326645b7769b0d111cb5086f64a5ae8
tushar090803 <tushargupta140507@gmail.com>
2026-07-03 18:25:42 +0530

    dsgrgbvwd

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,13 @@
+// create server
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
+const foodRoutes = require('./routes/food.routes');
+const foodPartnerRoutes = require('./routes/food-partner.routes');
+const cors = require('cors');
+
+const app = express();
+
 app.use(cors({
     // Dynamically allows ALL origins by echoing the incoming request's origin back
     origin: function (origin, callback) {
@@ -9,3 +19,16 @@ app.use(cors({
     },
     credentials: true // Keeps your cookie/JWT system working perfectly
 }));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.use('/api/auth', authRoutes);
+app.use('/api/food', foodRoutes);
+app.use('/api/food-partner', foodPartnerRoutes);
+
+module.exports = app;
